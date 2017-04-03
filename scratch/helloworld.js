1e0abec021cc7a4025292bdb9c96d77ff8b090ad
dennispedersen <dennispedersen@users.noreply.github.com>
2017-04-03 21:18:59 +0200

    Update helloworld.js

--- scratch/helloworld.js
+++ scratch/helloworld.js
@@ -1,6 +1,4 @@
-new (function() {
-    var ext = this;
-
+(function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -10,24 +8,18 @@ new (function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
-})();
+    ScratchExtensions.register('My first extension', descriptor, ext);
+})({});
